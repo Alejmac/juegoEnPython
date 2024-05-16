5aa39f531d7eafefa6cdbdd80d9ab94652b8f8af
Alejandro <alejmac2508@gmail.com>
2024-05-15 19:47:51 -0600

    Se agrego: icono del jugador

--- main.py
+++ main.py
@@ -9,13 +9,25 @@ pygame.display.set_caption("Invasion")
 #icono
 icono = pygame.image.load("sol.png")
 pygame.display.set_icon(icono)
+#jugador 
+img_jugador= pygame.image.load("nave-espacial.png")
+jugador_x= 0 
+jugador_y = 0
+
+#funcionpara mostrar jugador
+def jugador():
+    pantalla.blit(img_jugador,(jugador_x,jugador_y ))
 
 #loop del juego
 ejecutar = True
 while ejecutar:
+#cambiar el color de la pantalla
+    #se establece el relleno de la pantalla
+    pantalla.fill((205,144,228))
+
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             ejecutar = False
-    #cambiar el color de la pantalla
-    pantalla.fill((205,144,228))
+
+    jugador()
     pygame.display.update()
