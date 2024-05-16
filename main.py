a84ae139b052f2de15d9f1169a25203d1f3b59a6
Alejandro <alejmac2508@gmail.com>
2024-05-15 20:38:01 -0600

    Se agrego movimievnto en X y Y al jugador

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ icono = pygame.image.load("sol.png")
 pygame.display.set_icon(icono)
 #jugador 
 img_jugador= pygame.image.load("nave-espacial.png")
-jugador_x= 0 
-jugador_y = 0
+jugador_x= 368 
+jugador_y = 536
+jugador_x_cambio = 0
 
 #funcionpara mostrar jugador
-def jugador():
-    pantalla.blit(img_jugador,(jugador_x,jugador_y ))
+def jugador(x,y):
+    pantalla.blit(img_jugador,(x,y ))
 
 #loop del juego
 ejecutar = True
@@ -24,10 +25,22 @@ while ejecutar:
 #cambiar el color de la pantalla
     #se establece el relleno de la pantalla
     pantalla.fill((205,144,228))
+    #jugador_x += 0.1
 
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             ejecutar = False
+        #se activa a escucha de las teclas
+        if evento.type == pygame.KEYDOWN :
+            if evento.key == pygame.K_LEFT:
+                jugador_x_cambio = -0.1
+            if evento.key == pygame.K_RIGHT:
+                jugador_x_cambio = 0.1
 
-    jugador()
+        if evento.type == pygame.KEYUP:
+            if evento.key == pygame.K_LEFT or evento.key == pygame.K_RIGHT:
+                jugador_x_cambio = 0
+
+    jugador_x += jugador_x_cambio
+    jugador(jugador_x,jugador_y)
     pygame.display.update()
