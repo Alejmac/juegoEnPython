c79a71efe389526f83e86fe271004848ae7a703a
Alejandro <alejmac2508@gmail.com>
2024-05-27 18:57:33 -0600

    Se agrego al enemigo

--- main.py
+++ main.py
@@ -9,15 +9,27 @@ pygame.display.set_caption("Invasion")
 #icono
 icono = pygame.image.load("sol.png")
 pygame.display.set_icon(icono)
+
+
 #jugador 
 img_jugador= pygame.image.load("nave-espacial.png")
 jugador_x= 368 
 jugador_y = 536
 jugador_x_cambio = 0
 
+#variable enemigo 
+img_enemigo= pygame.image.load("enemigo.png")
+enemigo_x= 368 
+enemigo_y = 536
+enemigo_x_cambio = 0
+
 #funcionpara mostrar jugador
 def jugador(x,y):
     pantalla.blit(img_jugador,(x,y ))
+
+#funcionpara mostrar enemigo
+def enemigo(x,y):
+    pantalla.blit(img_enemigo,(x,y ))
 
 #loop del juego
 ejecutar = True
@@ -28,19 +40,31 @@ while ejecutar:
     #jugador_x += 0.1
 
     for evento in pygame.event.get():
+        #evento para cerrar el programa 
         if evento.type == pygame.QUIT:
             ejecutar = False
-        #se activa a escucha de las teclas
+        #eventi oresionar una tecla
         if evento.type == pygame.KEYDOWN :
             if evento.key == pygame.K_LEFT:
                 jugador_x_cambio = -0.1
             if evento.key == pygame.K_RIGHT:
                 jugador_x_cambio = 0.1
-
+        #evento solar teclas 
         if evento.type == pygame.KEYUP:
             if evento.key == pygame.K_LEFT or evento.key == pygame.K_RIGHT:
                 jugador_x_cambio = 0
 
     jugador_x += jugador_x_cambio
+    #mantener bordes 
+    if jugador_x <= 0:
+        jugador_x = 0
+    elif jugador_x >= 736:
+        jugador_x = 736
+
+
     jugador(jugador_x,jugador_y)
+    enemigo(jugador_x,jugador_y)
+
+ 
+    #actualizar
     pygame.display.update()
